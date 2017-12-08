2817702889ac4afa2a6fbc6fc7fc3142b90bc29e
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2017-12-08 17:18:24 +0100

    Created interface to define service methods

---
+++ app/src/main/java/com/joseangelmaneiro/movies/data/source/remote/ApiEndpointInterface.java
@@ -0,0 +1,14 @@
+package com.joseangelmaneiro.movies.data.source.remote;
+
+import com.joseangelmaneiro.movies.data.Page;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface ApiEndpointInterface {
+
+    @GET("movie/popular")
+    Call<Page> getMovies(@Query("api_key") String apiKey);
+
+}
