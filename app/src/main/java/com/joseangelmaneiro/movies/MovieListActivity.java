b839a61f81188e9258fc4337ab2796cac16e8056
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2017-12-18 20:14:45 +0100

    Configured toolbar in movie list screen

--- app/src/main/java/com/joseangelmaneiro/movies/MovieListActivity.java
+++ app/src/main/java/com/joseangelmaneiro/movies/MovieListActivity.java
@@ -2,6 +2,8 @@ package com.joseangelmaneiro.movies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        setUpToolbar();
+    }
+
+    private void setUpToolbar(){
+        Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
