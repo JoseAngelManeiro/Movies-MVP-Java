b09ca084a4ac234ccd3ef600746cf6da96fd0757
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2017-12-26 08:51:00 +0100

    Created class to add margin to items

---
+++ app/src/main/java/com/joseangelmaneiro/movies/ui/list/MarginDecoration.java
@@ -0,0 +1,24 @@
+package com.joseangelmaneiro.movies.ui.list;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import com.joseangelmaneiro.movies.R;
+
+
+public class MarginDecoration extends RecyclerView.ItemDecoration {
+
+    private int margin;
+
+    public MarginDecoration(Context context) {
+        margin = context.getResources().getDimensionPixelSize(R.dimen.item_margin);
+    }
+
+    @Override
+    public void getItemOffsets(
+            Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.set(margin, margin, margin, margin);
+    }
+
+}
