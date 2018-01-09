2366601ddf80e77cf4a005193bb63276cb066a64
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-01-09 18:13:29 +0100

    Created DetailMoviePresenter

---
+++ app/src/main/java/com/joseangelmaneiro/movies/ui/detail/DetailMoviePresenter.java
@@ -0,0 +1,52 @@
+package com.joseangelmaneiro.movies.ui.detail;
+
+import com.joseangelmaneiro.movies.data.Handler;
+import com.joseangelmaneiro.movies.data.Movie;
+import com.joseangelmaneiro.movies.data.MoviesRepository;
+import com.joseangelmaneiro.movies.ui.Formatter;
+import java.lang.ref.WeakReference;
+
+
+public class DetailMoviePresenter implements Handler<Movie>{
+
+    private MoviesRepository repository;
+
+    private Formatter formatter;
+
+    private int movieId;
+
+    private WeakReference<DetailMovieView> view;
+
+
+    public DetailMoviePresenter(MoviesRepository repository, Formatter formatter, int movieId){
+        this.repository = repository;
+        this.formatter = formatter;
+        this.movieId = movieId;
+    }
+
+    public void setView(DetailMovieView detailMovieView){
+        view = new WeakReference<>(detailMovieView);
+    }
+
+    public void viewReady(){
+        repository.getMovie(movieId, this);
+    }
+
+    @Override
+    public void handle(Movie movie) {
+        DetailMovieView detailMovieView = view.get();
+        if(detailMovieView!=null){
+            detailMovieView.displayImage(formatter.getCompleteUrlImage(movie.getBackdropPath()));
+            detailMovieView.displayTitle(movie.getTitle());
+            detailMovieView.displayVoteAverage(formatter.getVoteAverage(movie.getVoteAverage()));
+            detailMovieView.displayReleaseDate(movie.getReleaseDate());
+            detailMovieView.displayOverview(movie.getOverview());
+        }
+    }
+
+    @Override
+    public void error() {
+    }
+
+
+}
