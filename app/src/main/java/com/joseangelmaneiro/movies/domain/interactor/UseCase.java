fd6f5c30e3420f04961d39d43c2510d82096f255
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-01-20 23:23:00 +0100

    Updated UseCase

--- app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCase.java
+++ app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCase.java
@@ -1,6 +1,10 @@
 package com.joseangelmaneiro.movies.domain.interactor;
 
+import com.joseangelmaneiro.movies.domain.Handler;
 
-public interface UseCase {
-    void execute();
+
+public interface UseCase<T, P> {
+
+    void execute(Handler<T> handler, P params);
+
 }
