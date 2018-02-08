2cc2f21996ae5f9ffa4f9e1707eeb3adbb078b04
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-02-08 19:39:58 +0100

    Added method to provide UseCaseFactory dependency

--- app/src/main/java/com/joseangelmaneiro/movies/di/Injection.java
+++ app/src/main/java/com/joseangelmaneiro/movies/di/Injection.java
@@ -2,7 +2,6 @@ package com.joseangelmaneiro.movies.di;
 
 import android.content.Context;
 import com.joseangelmaneiro.movies.data.entity.mapper.EntityDataMapper;
-import com.joseangelmaneiro.movies.domain.MoviesRepository;
 import com.joseangelmaneiro.movies.data.MoviesRepositoryImpl;
 import com.joseangelmaneiro.movies.data.source.local.MoviesLocalDataSource;
 import com.joseangelmaneiro.movies.data.source.local.MoviesLocalDataSourceImpl;
@@ -10,13 +9,16 @@ import com.joseangelmaneiro.movies.data.source.local.db.MoviesDatabaseHelper;
 import com.joseangelmaneiro.movies.data.source.remote.MoviesRemoteDataSource;
 import com.joseangelmaneiro.movies.data.source.remote.MoviesRemoteDataSourceImpl;
 import com.joseangelmaneiro.movies.data.source.remote.net.RetrofitClient;
+import com.joseangelmaneiro.movies.domain.interactor.UseCaseFactory;
 
 
 public class Injection {
 
-    public static MoviesRepository provideRepository(Context context){
-        return MoviesRepositoryImpl.getInstance(getLocalDataSource(context),
-                getRemoteDataSource(), new EntityDataMapper());
+    public static UseCaseFactory provideUseCaseFactory(Context context){
+        return new UseCaseFactory(MoviesRepositoryImpl.getInstance(
+                getLocalDataSource(context),
+                getRemoteDataSource(),
+                new EntityDataMapper()));
     }
 
     private static MoviesLocalDataSource getLocalDataSource(Context context){
