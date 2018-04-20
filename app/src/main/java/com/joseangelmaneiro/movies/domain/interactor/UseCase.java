9d95069aafe1223f5b9a65e7f1473718fb7b3bab
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-04-20 19:40:17 +0200

    Converted UseCase into a class that builds and adds the observables into a CompositeDisposable

--- app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCase.java
+++ app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCase.java
@@ -1,10 +1,42 @@
 package com.joseangelmaneiro.movies.domain.interactor;
 
-import com.joseangelmaneiro.movies.domain.Handler;
+import com.joseangelmaneiro.movies.domain.executor.JobScheduler;
+import com.joseangelmaneiro.movies.domain.executor.UIScheduler;
+import io.reactivex.Single;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableSingleObserver;
 
 
-public interface UseCase<T, P> {
+public abstract class UseCase<Observer, Params> {
 
-    void execute(Handler<T> handler, P params);
+    private final UIScheduler uiScheduler;
+    private final JobScheduler jobScheduler;
+    private final CompositeDisposable disposables;
+
+    UseCase(UIScheduler uiScheduler, JobScheduler jobScheduler) {
+        this.uiScheduler = uiScheduler;
+        this.jobScheduler = jobScheduler;
+        this.disposables = new CompositeDisposable();
+    }
+
+    abstract Single<Observer> buildUseCaseObservable(Params params);
+
+    public void execute(DisposableSingleObserver<Observer> observer, Params params) {
+        final Single<Observer> observable = this.buildUseCaseObservable(params)
+                .observeOn(uiScheduler.getScheduler())
+                .subscribeOn(jobScheduler.getScheduler());
+        addDisposable(observable.subscribeWith(observer));
+    }
+
+    public void dispose() {
+        if (!disposables.isDisposed()) {
+            disposables.dispose();
+        }
+    }
+
+    private void addDisposable(Disposable disposable) {
+        disposables.add(disposable);
+    }
 
 }
