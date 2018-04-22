193cf7da5a409f69d1c42730e2a92abb1f8a316d
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-04-22 09:32:39 +0200

    Modified remote implementation to work synchronously

--- app/src/main/java/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSource.java
+++ app/src/main/java/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSource.java
@@ -1,12 +1,11 @@
 package com.joseangelmaneiro.movies.data.source.remote;
 
-import com.joseangelmaneiro.movies.domain.Handler;
 import com.joseangelmaneiro.movies.data.entity.MovieEntity;
 import java.util.List;
 
 
 public interface MoviesRemoteDataSource {
 
-    void getAll(Handler<List<MovieEntity>> handler);
+    List<MovieEntity> getAll() throws Exception;
 
 }
--- app/src/main/java/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSourceImpl.java
+++ app/src/main/java/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSourceImpl.java
@@ -2,14 +2,11 @@ package com.joseangelmaneiro.movies.data.source.remote;
 
 import com.joseangelmaneiro.movies.data.exception.NetworkConnectionException;
 import com.joseangelmaneiro.movies.data.exception.ServiceException;
-import com.joseangelmaneiro.movies.domain.Handler;
 import com.joseangelmaneiro.movies.data.entity.MovieEntity;
 import com.joseangelmaneiro.movies.data.entity.PageEntity;
-
+import java.io.IOException;
 import java.util.List;
 import javax.inject.Inject;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 
@@ -18,30 +15,25 @@ public class MoviesRemoteDataSourceImpl implements MoviesRemoteDataSource {
     // TODO Put here your api key (https://developers.themoviedb.org/3/getting-started)
     private static final String API_KEY = "";
 
-    private MovieService movieService;
+    private MovieService service;
 
     @Inject
-    public MoviesRemoteDataSourceImpl(MovieService movieService) {
-        this.movieService = movieService;
+    public MoviesRemoteDataSourceImpl(MovieService service) {
+        this.service = service;
     }
 
     @Override
-    public void getAll(final Handler<List<MovieEntity>> handler) {
-        movieService.getPageEntity(API_KEY).enqueue(new Callback<PageEntity>() {
-            @Override
-            public void onResponse(Call<PageEntity> call, Response<PageEntity> response) {
-                if(response.isSuccessful()){
-                    handler.handle(response.body().movies);
-                } else {
-                    handler.error(new ServiceException());
-                }
+    public List<MovieEntity> getAll() throws Exception {
+        try {
+            Response<PageEntity> response = service.getPageEntity(API_KEY).execute();
+            if(response.isSuccessful()){
+                return response.body().movies;
+            } else{
+                throw new ServiceException();
             }
-
-            @Override
-            public void onFailure(Call<PageEntity> call, Throwable t) {
-                handler.error(new NetworkConnectionException());
-            }
-        });
+        } catch (IOException exception) {
+            throw new NetworkConnectionException();
+        }
     }
 
 }
