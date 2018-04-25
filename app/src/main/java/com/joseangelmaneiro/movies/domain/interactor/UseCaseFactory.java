900f2a0f4d10de355303797d9083714aefc8eff8
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-04-25 17:22:20 +0200

    Added schedulers to use case factory

--- app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCaseFactory.java
+++ app/src/main/java/com/joseangelmaneiro/movies/domain/interactor/UseCaseFactory.java
@@ -1,24 +1,32 @@
 package com.joseangelmaneiro.movies.domain.interactor;
 
 import com.joseangelmaneiro.movies.domain.MoviesRepository;
+import com.joseangelmaneiro.movies.domain.executor.JobScheduler;
+import com.joseangelmaneiro.movies.domain.executor.UIScheduler;
 import javax.inject.Inject;
 
 
 public class UseCaseFactory {
 
     private MoviesRepository repository;
+    private UIScheduler uiScheduler;
+    private JobScheduler jobScheduler;
 
     @Inject
-    public UseCaseFactory(MoviesRepository repository){
+    public UseCaseFactory(MoviesRepository repository,
+                          UIScheduler uiScheduler,
+                          JobScheduler jobScheduler){
         this.repository = repository;
+        this.uiScheduler = uiScheduler;
+        this.jobScheduler = jobScheduler;
     }
 
     public UseCase getMovie(){
-        return new GetMovie(repository);
+        return new GetMovie(repository, uiScheduler, jobScheduler);
     }
 
     public UseCase getMovies(){
-        return new GetMovies(repository);
+        return new GetMovies(repository, uiScheduler, jobScheduler);
     }
 
 }
