2220e1e5fe3e7d977d7d9692b23519e9fb1dddbc
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-05-28 18:52:12 +0200

    Created utility class for integration tests

---
+++ app/src/test/java/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSourceImplTest.java
@@ -0,0 +1,17 @@
+package com.joseangelmaneiro.movies.data.source.remote;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MoviesRemoteDataSourceImplTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
